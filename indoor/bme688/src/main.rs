1fc81e4ac5cde109601c0e027186078c7a717bd4
peter <peter@gandalf>
2023-09-21 23:08:02 +0100

    more updates

--- indoor/bme688/src/main.rs
+++ indoor/bme688/src/main.rs
@@ -7,13 +7,24 @@ const BME688_ADDR : u16 = 0x76;
 
 
 struct Bme688 {
-    mDev : LinuxI2CDevice,
-    mHumOversampling : u8,
-    mTempOversampling : u8,
-    mPresOversampling : u8,
-    par_t1 : i32,
-    par_t2 : i32,
-    par_t3 : i32
+    dev : LinuxI2CDevice,
+    hum_oversampling : u8,
+    temp_oversampling : u8,
+    pres_oversampling : u8,
+    par_t1 : u16,
+    par_t2 : i16,
+    par_t3 : i8,
+    par_p1 : u16,
+    par_p2 : i16,
+    par_p3 : i8,
+    par_p4 : i16,
+    par_p5 : i16,
+    par_p6 : i8,
+    par_p7 : i8,
+    par_p8 : i16,
+    par_p9 : i16,
+    par_p10 : u8,
+    t_fine : f64
 }
 
 
@@ -23,7 +34,7 @@ enum Bme688Error {
 }
 
 
-fn calcOversampling(reqd : u8) -> Result<u8, Bme688Error> {
+fn calc_oversampling(reqd : u8) -> Result<u8, Bme688Error> {
     let result;
     if reqd == 1 {
         result =  1;  // no oversampling
@@ -44,62 +55,192 @@ fn calcOversampling(reqd : u8) -> Result<u8, Bme688Error> {
 
 impl Bme688 {
 
-    fn new() -> Result<Bme688,LinuxI2CError> {
+    fn new() -> Result<Self,LinuxI2CError> {
         let mut dev = LinuxI2CDevice::new("/dev/i2c-4", BME688_ADDR)?;
 
-        let par_t1 = (i32::from(dev.smbus_read_byte_data(0xEA).unwrap()) << 8)
-            + i32::from(dev.smbus_read_byte_data(0xE9).unwrap());
+        let par_t1 = ((dev.smbus_read_byte_data(0xEA).unwrap() as u16) << 8)
+            | (dev.smbus_read_byte_data(0xE9).unwrap() as u16);
 
-        let par_t2 = (i32::from(dev.smbus_read_byte_data(0x8B).unwrap()) << 8)
-            + i32::from(dev.smbus_read_byte_data(0x8A).unwrap());
+        let par_t2 = (((dev.smbus_read_byte_data(0x8B).unwrap() as u16) << 8)
+            | (dev.smbus_read_byte_data(0x8A).unwrap() as u16)) as i16;
 
-        let par_t3 = i32::from(dev.smbus_read_byte_data(0x8C).unwrap());
+        let par_t3 = dev.smbus_read_byte_data(0x8C).unwrap() as i8;
 
         println!("par_t1 {:?}", par_t1);
         println!("par_t2 {:?}", par_t2);
         println!("par_t3 {:?}", par_t3);
 
-        let bme688 = Bme688 {
-            mDev : dev,
-            mHumOversampling : 0,
-            mTempOversampling : 0,
-            mPresOversampling : 0,
+        let par_p1 = ((dev.smbus_read_byte_data(0x8F).unwrap() as u16) << 8)
+            | (dev.smbus_read_byte_data(0x8E).unwrap() as u16);
+
+        let par_p2 = (((dev.smbus_read_byte_data(0x91).unwrap() as u16) << 8)
+            | (dev.smbus_read_byte_data(0x90).unwrap() as u16)) as i16;
+
+        let par_p3 = dev.smbus_read_byte_data(0x92).unwrap() as i8;
+
+        let par_p4 = (((dev.smbus_read_byte_data(0x95).unwrap() as u16) << 8)
+            | (dev.smbus_read_byte_data(0x94).unwrap() as u16)) as i16;
+
+        let par_p5 = (((dev.smbus_read_byte_data(0x97).unwrap() as u16) << 8)
+            | (dev.smbus_read_byte_data(0x96).unwrap() as u16)) as i16;
+
+        let par_p6 = dev.smbus_read_byte_data(0x99).unwrap() as i8;
+
+        let par_p7 = dev.smbus_read_byte_data(0x98).unwrap() as i8;
+
+        let par_p8 = (((dev.smbus_read_byte_data(0x9D).unwrap() as u16) << 8)
+            | (dev.smbus_read_byte_data(0x9C).unwrap() as u16)) as i16;
+
+        let par_p9 = (((dev.smbus_read_byte_data(0x9F).unwrap() as u16) << 8)
+            | (dev.smbus_read_byte_data(0x9E).unwrap() as u16)) as i16;
+
+        let par_p10 = dev.smbus_read_byte_data(0xA0).unwrap();
+
+        println!("par_p1 {:?}", par_p1);
+        println!("par_p2 {:?}", par_p2);
+        println!("par_p3 {:?}", par_p3);
+        println!("par_p4 {:?}", par_p4);
+        println!("par_p5 {:?}", par_p5);
+        println!("par_p6 {:?}", par_p6);
+        println!("par_p7 {:?}", par_p7);
+        println!("par_p8 {:?}", par_p8);
+        println!("par_p9 {:?}", par_p9);
+        println!("par_p10 {:?}", par_p10);
+
+        let this = Self {
+            dev,
+            hum_oversampling : 0,
+            temp_oversampling : 0,
+            pres_oversampling : 0,
             par_t1,
             par_t2,
-            par_t3
+            par_t3,
+            par_p1, par_p2, par_p3, par_p4, par_p5, par_p6, par_p7, par_p8, par_p9, par_p10,
+            t_fine : 0.0
         };
-        Ok(bme688)
+        Ok(this)
     }
 
 
     fn set_humdity_oversampling(&mut self, oversampling : u8) {
-        self.mHumOversampling = calcOversampling(oversampling).unwrap();
+        self.hum_oversampling = calc_oversampling(oversampling).unwrap();
     }
 
 
     fn set_temperature_oversampling(&mut self, oversampling : u8) {
-        self.mTempOversampling = calcOversampling(oversampling).unwrap();
+        self.temp_oversampling = calc_oversampling(oversampling).unwrap();
     }
 
 
     fn set_pressure_oversampling(&mut self, oversampling : u8) {
-        self.mPresOversampling = calcOversampling(oversampling).unwrap();
+        self.pres_oversampling = calc_oversampling(oversampling).unwrap();
+    }
+
+    fn read_temp_adc(&mut self, field :u8) -> u32 {
+        let base : u8 = 0x22 + 0x11 * field;
+        let msb = self.dev.smbus_read_byte_data(base + 0x00).unwrap() as u32;
+        let lsb = self.dev.smbus_read_byte_data(base + 0x01).unwrap() as u32;
+        let xlsb = self.dev.smbus_read_byte_data(base + 0x02).unwrap() as u32;
+
+        let adc = (msb << 12) | (lsb << 4) | (xlsb >> 4);
+        println!("temp_adc {:?}", adc);
+        adc
+    }
+
+    fn read_press_adc(&mut self, field :u8) -> u32 {
+        let base : u8 = 0x1F + 0x11 * field;
+        let msb = self.dev.smbus_read_byte_data(base + 0x00).unwrap() as u32;
+        let lsb = self.dev.smbus_read_byte_data(base + 0x01).unwrap() as u32;
+        let xlsb = self.dev.smbus_read_byte_data(base + 0x02).unwrap() as u32;
+
+        let adc = (msb << 12) | (lsb << 4) | (xlsb >> 4);
+        println!("press_adc {:?}", adc);
+        adc
+    }
+
+    fn read_humd_adc(&mut self, field :u8) -> u16 {
+        let base : u8 = 0x25 + 0x11 * field;
+        let msb = self.dev.smbus_read_byte_data(base + 0x00).unwrap() as u16;
+        let lsb = self.dev.smbus_read_byte_data(base + 0x01).unwrap() as u16;
+
+        let adc = (msb << 8) | lsb;
+        println!("humd_adc {:?}", adc);
+        adc
+    }
+
+
+    fn read_temp(&mut self, field :u8) -> f64 {
+
+        let temp_adc = f64::from(self.read_temp_adc(field));
+        let par_t1 = f64::from(self.par_t1);
+        let par_t2 = f64::from(self.par_t2);
+        let par_t3 = f64::from(self.par_t3);
+
+        let var1 = ((temp_adc / 16384.0) - (par_t1 / 1024.0)) * par_t2;
+        println!("var1 {:?}", var1);
+
+        let var2 = (((temp_adc / 131072.0) - (par_t1 / 8192.0)) *
+            ((temp_adc / 131072.0) - (par_t1 / 8192.0))) *
+            (par_t3 * 16.0);
+
+        let t_fine = var1 + var2;
+
+        self.t_fine = t_fine;
+
+        let temp = t_fine / 5120.0;
+        println!("temp_comp {:?}", temp);
+        temp
+    }
+
+
+    fn read_press(&mut self, field: u8) -> f64 {
+
+        let press_adc = f64::from(self.read_press_adc(field));
+        let par_p1 = f64::from(self.par_p1);
+        let par_p2 = f64::from(self.par_p2);
+        let par_p3 = f64::from(self.par_p3);
+        let par_p4 = f64::from(self.par_p4);
+        let par_p5 = f64::from(self.par_p5);
+        let par_p6 = f64::from(self.par_p6);
+        let par_p7 = f64::from(self.par_p7);
+        let par_p8 = f64::from(self.par_p8);
+        let par_p9 = f64::from(self.par_p9);
+        let par_p10 = f64::from(self.par_p10);
+
+        let var1 = (self.t_fine / 2.0) - 64000.0;
+        let var2 = var1 * var1 * (par_p6 / 131072.0);
+
+        let var2 = var2 + (var1 * par_p5 * 2.0);
+        let var2 = (var2 / 4.0) + (par_p4 * 65536.0);
+        let var1 = (((par_p3 * var1 * var1) / 16384.0) + (par_p2 * var1)) / 524288.0;
+
+        let var1 = (1.0 + (var1 / 32768.0)) * par_p1;
+        let press_comp = 1048576.0 - press_adc;
+        let press_comp = ((press_comp - (var2 / 4096.0)) * 6250.0) / var1;
+        let var1 = (par_p9 * press_comp * press_comp) / 2147483648.0;
+        let var2 = press_comp * (par_p8 / 32768.0);
+        let var3 = (press_comp / 256.0) * (press_comp / 256.0) *
+            (press_comp / 256.0) * (par_p10 / 131072.0);
+        let press_comp = press_comp + (var1 + var2 + var3 +
+            (par_p7 * 128.0)) / 16.0;
+        println!("press_comp {:?}", press_comp);
+        press_comp
     }
 
 
     fn force(&mut self) {
         // Write Humdity oversampling
 
-        self.mDev.smbus_write_byte_data(0x72, self.mHumOversampling);
+        self.dev.smbus_write_byte_data(0x72, self.hum_oversampling).unwrap();
 
         // Write Pressure & temperature oversampling
-        let tmp = (self.mTempOversampling << 5) | (self.mPresOversampling << 2);
-        self.mDev.smbus_write_byte_data(0x74, tmp);
+        let tmp = (self.temp_oversampling << 5) | (self.pres_oversampling << 2);
+        self.dev.smbus_write_byte_data(0x74, tmp).unwrap();
 
-        self.mDev.smbus_write_byte_data(0x74, tmp | 1);
+        self.dev.smbus_write_byte_data(0x74, tmp | 1).unwrap();
 
         loop {
-            let mode = self.mDev.smbus_read_byte_data(0x74).unwrap() & 0x03;
+            let mode = self.dev.smbus_read_byte_data(0x74).unwrap() & 0x03;
             if mode == 0 {
                 break;
             }
@@ -112,55 +253,12 @@ fn main() {
 
     let mut drv =  Bme688::new().unwrap();
 
-
     drv.set_humdity_oversampling(1);
     drv.set_pressure_oversampling(16);
     drv.set_temperature_oversampling(2);
 
     drv.force();
 
-    let mut dev = LinuxI2CDevice::new("/dev/i2c-4", BME688_ADDR).unwrap();
-
-    let mut temp_adc = i32::from(dev.smbus_read_byte_data(0x22).unwrap());
-    temp_adc = (temp_adc << 8) + i32::from(dev.smbus_read_byte_data(0x23).unwrap());
-    temp_adc = (temp_adc << 4) + (i32::from(dev.smbus_read_byte_data(0x24).unwrap()) >> 4);
-    println!("temp_adc {:?}", temp_adc);
-
-    let mut par_t1 = i32::from(dev.smbus_read_byte_data(0xEA).unwrap());
-    par_t1 = (par_t1 << 8) + i32::from(dev.smbus_read_byte_data(0xE9).unwrap());
-    println!("par_t1 {:?}", par_t1);
-
-    let mut par_t2 = i32::from(dev.smbus_read_byte_data(0x8B).unwrap());
-    par_t2 = (par_t2 << 8) + i32::from(dev.smbus_read_byte_data(0x8A).unwrap());
-    println!("par_t2 {:?}", par_t2);
-
-    let par_t3 = i32::from(dev.smbus_read_byte_data(0x8C).unwrap());
-    println!("par_t3 {:?}", par_t3);
-
-
-//    let var1 = ((f64::from(temp_adc) / 16384.0) - (f64::from(par_t1) / 1024.0)) * f64::from(par_t2);
-//    println!("var1 {:?}", var1);
-
-//    let var2 = (((f64::from(temp_adc) / 131072.0) - (f64::from(par_t1) / 8192.0)) *
-//        ((f64::from(temp_adc) / 131072.0) - (f64::from(par_t1) / 8192.0))) *
-//        (f64::from(par_t3) * 16.0);
-//    let t_fine = var1 + var2;
-//    let temp_comp = t_fine / 5120.0;
-//    println!("temp_comp {:?}", temp_comp);
-
-    let var1 = (temp_adc >> 3) - (par_t1 << 1);
-    println!("var1 {:?}", var1);
-
-    let var2 = (i64::from(var1) * i64::from(par_t2)) >> 11;
-    println!("var2 {:?}", var2);
-
-    let var3 = (((i64::from(var1 >> 1) * i64::from(var1 >> 1)) >> 12) * i64::from(par_t3 << 4)) >> 14;
-    println!("var3 {:?}", var3);
-
-    let t_fine = var2 + var3;
-    println!("t_fine {:?}", t_fine);
-
-    let temp_comp = ((t_fine * 5) + 128) >> 8;
-    println!("temp_comp {:?}", temp_comp);
-
+    let temp = drv.read_temp(0);
+    let temp = drv.read_press(0);
 }
