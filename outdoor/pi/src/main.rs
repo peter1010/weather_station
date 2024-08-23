8d76201e5af7f68edb118be930ca870fee3a5cb3
Peter <peter@eowyn>
2024-08-23 08:39:28 +0100

    fixed async issues

--- outdoor/pi/src/main.rs
+++ outdoor/pi/src/main.rs
@@ -1,6 +1,11 @@
 use toml::Table;
 use tokio::fs::File;
 use tokio::io::{self, BufReader, AsyncBufReadExt};
+use tokio::time::sleep;
+use chrono;
+use chrono::{DateTime, Utc};
+use chrono::Timelike;
+use std::time::Duration;
 
 struct Wind {
     max_speed : f32,
@@ -11,17 +16,14 @@ struct Wind {
 }
 
 impl Wind {
-    pub fn new(dev_name : &str) -> Self {
-        let tmp = Wind {
-            max_speed : 0.0,
-            min_speed : 0.0,
-            sum : 0.0,
-            num_of : 0,
-            dev_name : dev_name.to_string()
-        };
-        tmp
+    fn init(&mut self, dev_name : &str) {
+        self.dev_name = dev_name.to_string();
+        self.reset();
     }
 
+    fn reset(&mut self) {
+        self.num_of = 0;
+    }
 
     fn process(&mut self, speed : f32) {
         if self.num_of > 0 {
@@ -32,13 +34,20 @@ impl Wind {
                     self.min_speed = speed;
                 }
             }
+            self.num_of += 1;
+            self.sum += speed as f64;
         } else {
             self.max_speed = speed;
             self.min_speed = speed;
+            self.sum = speed as f64;
+            self.num_of = 1;
         }
-        self.num_of += 1;
-        self.sum += speed as f64;
-        println!("{} {} {}", self.max_speed, self.sum / (self.num_of as f64), self.min_speed);
+    }
+
+    fn save(&mut self) {
+        let now = chrono::Utc::now();
+        println!("{} {} {} {}", now, self.max_speed, self.sum / (self.num_of as f64), self.min_speed);
+        self.num_of = 0;
     }
 
     async fn task(&mut self) -> io::Result<()> {
@@ -58,6 +67,39 @@ impl Wind {
     }
 }
 
+static mut g_wind : Wind = Wind {
+            max_speed : 0.0,
+            min_speed : 0.0,
+            sum : 0.0,
+            num_of : 0,
+            dev_name : String::new()
+};
+
+
+struct Clock {
+    end_time : DateTime<Utc>
+}
+
+
+async fn clock() -> Result<(), ()> {
+     let now = chrono::Utc::now();
+     let min = now.minute();
+     let sec = now.second();
+     let mut delay = 60 - sec;
+     if min < 15 {
+         delay += (15 - min) * 60;
+      } else if min < 30 {
+         delay += (30 - min) * 60;
+      } else if min < 45 {
+         delay += (45 - min) * 60;
+      } else {
+         delay += (60 - min) * 60;
+      }
+        println!("Duration {}", delay);
+     sleep(Duration::from_secs(delay.into())).await;
+     Ok(())
+}
+
 
 fn main() -> Result<(), ()> {
     let path = std::path::Path::new("outdoor.toml");
@@ -69,15 +111,27 @@ fn main() -> Result<(), ()> {
     let config: Table = config_str.parse().unwrap();
 
     let dev_name = config["Wind"]["dev"].as_str().unwrap();
-    let mut wind = Wind::new(dev_name);
+
+    unsafe {
+        g_wind.init(dev_name);
+    }
 
     println!("Hello, world!");
     dbg!(&config);
 
     let mut rt = tokio::runtime::Runtime::new().unwrap();
 
-    let task = wind.task();
-    rt.block_on(task);
+
+    unsafe {
+        rt.spawn(g_wind.task());
+    }
+    loop {
+        rt.block_on(clock());
+        println!("Tick");
+        unsafe {
+            g_wind.save();
+        }
+    }
 
     Ok(())
 }
