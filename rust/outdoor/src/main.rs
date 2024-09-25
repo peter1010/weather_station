bfb1c6029aef174c63afb603f374496c9c1dd8ff
Peter <peter@eowyn>
2024-09-25 21:55:55 +0100

    use listener library

--- rust/outdoor/src/main.rs
+++ rust/outdoor/src/main.rs
@@ -3,102 +3,17 @@
 //!
 
 use toml::Table;
-use tokio::net::TcpListener;
 use tokio::time::sleep;
 use std::time::Duration;
-use sqlite:: Connection;
-use tokio::io::{self,AsyncBufReadExt,AsyncWriteExt, BufReader};
 use std::sync::{Arc, Mutex};
 
 use clock;
+use listener::Listener;
 
 use crate::wind::Wind;
 
 mod stats;
 mod wind;
-
-struct Listener {
-    port : u16,
-    db_connection : Arc<Mutex<Connection>>
-}
-
-impl Listener {
-
-    pub fn new(port :u16, db_connection : Arc<Mutex<Connection>>) -> Listener {
-        Listener {
-            port,
-            db_connection
-        }
-    }
-
-
-    pub async fn task(&mut self) -> io::Result<()> {
-
-        let sock_addr = format!("0.0.0.0:{}", self.port);
-
-        // Next up we create a TCP listener which will listen for incoming
-        // connections. This TCP listener is bound to the address we determined
-        // above and must be associated with an event loop.
-        let listener = TcpListener::bind(&sock_addr).await?;
-        println!("Listening on: {}", sock_addr);
-
-        loop {
-            // Asynchronously wait for an inbound socket.
-            let (socket, _) = listener.accept().await?;
-
-            let mut stream = BufReader::new(socket);
-
-
-            // In a loop, read data from the socket and write the data back.
-            loop {
-                let mut line = String::new();
-
-                let n = stream
-                    .read_line(&mut line)
-                    .await
-                    .expect("failed to read data from socket");
-
-                if n == 0 {
-                    break;
-                }
-
-                let unix_time = line.trim().parse::<i64>();
-                if !unix_time.is_ok() {
-                    continue;
-                }
-                let unix_time = unix_time.unwrap();
-
-                println!("Rcv'd {:?}", unix_time);
-
-                let mut response = String::new();
-
-                let query = format!("select * from Outdoor where unix_time > {};", unix_time);
-                {
-                    let conn = self.db_connection.lock().unwrap();
-
-                    let statement = (*conn).prepare(query).unwrap();
-
-                    // unix_time INT, max REAL, ave REAL, min REAL
-
-
-                    for row in statement
-                        .into_iter()
-                        .map(|row| row.unwrap())
-                    {
-                        response += &(format!("unix_time = {}", row.read::<i64, _>("unix_time")) + "\n");
-                        response += &(format!("\tmax = {}", row.read::<f64, _>("max")) + "\n");
-                        response += &(format!("\tave = {}", row.read::<f64, _>("ave")) + "\n");
-                        response += &(format!("\tmin = {}", row.read::<f64, _>("min")) + "\n");
-                    }
-                }
-
-                stream.write_all(response.as_bytes()).await
-                    .expect("failed to write data to socket");
-            }
-        }
-    }
-}
-
 
 
 async fn wait_tick(ticker : &clock::Clock) -> Result<(), ()> {
