8c92dfc65f20dd8a3868aa5162961cacc2fb6b5e
Peter Leese <peter1010@users.noreply.github.com>
2024-10-08 00:08:50 +0100

    Simplify main in indoor project

--- rust/indoor/src/main.rs
+++ rust/indoor/src/main.rs
@@ -12,8 +12,8 @@ use listener::Listener;
 type Connection = Arc<Mutex<sqlite::Connection>>;
 
 async fn wait_tick(ticker : &clock::Clock) -> Result<(), ()> {
-     let delay = ticker.secs_to_next_tick();
-     sleep(Duration::from_secs(delay.into())).await;
+     let delay_seconds = ticker.secs_to_next_tick();
+     sleep(Duration::from_secs(delay_seconds.into())).await;
      Ok(())
 }
 
@@ -27,21 +27,30 @@ fn launch_listener(config : &Table, rt : &Runtime, db_connection : Connection)
 }
 
 
-fn main() {
-
-    let path = std::path::Path::new("weather.toml");
-    let config_str = match std::fs::read_to_string(path) {
-        Ok(f) => f,
-        Err(e) => panic!("Failed to read config file {}", e)
-    };
-
-    let config: Table = config_str.parse().unwrap();
-    // dbg!(&config);
+fn create_sensor(config : &Table) -> Bme688 {
 
     let dev_name = config["indoor"]["dev"].as_str().unwrap();
     println!("Reading from {} for indoor sensor", dev_name);
 
     let mut sensor =  Bme688::new(dev_name).unwrap();
+
+    sensor.cache_params();
+
+    sensor.set_humdity_oversampling(16);
+    sensor.set_pressure_oversampling(16);
+    sensor.set_temperature_oversampling(16);
+
+    sensor
+}
+
+
+fn create_ticker(config : &Table) -> clock::Clock {
+    let period = config["common"]["sample_period_in_mins"].as_integer().unwrap() as i32;
+    clock::Clock::new(period * 60)
+}
+
+
+fn create_db_connection(config : &Table)-> (Connection, String) {
 
     let db_file = config["indoor"]["database"].as_str().unwrap();
     println!("Opening database {}", db_file);
@@ -56,16 +65,26 @@ fn main() {
         let conn = db_connection.lock().unwrap();
         (*conn).execute(query).unwrap();
     }
-
-    sensor.cache_params();
-
-    sensor.set_humdity_oversampling(16);
-    sensor.set_pressure_oversampling(16);
-    sensor.set_temperature_oversampling(16);
+    (db_connection, String::from(db_table))
+}
 
 
-    let period = config["common"]["sample_period_in_mins"].as_integer().unwrap() as i32;
-    let ticker = clock::Clock::new(period * 60);
+fn main() {
+
+    let path = std::path::Path::new("weather.toml");
+    let config_str = match std::fs::read_to_string(path) {
+        Ok(f) => f,
+        Err(e) => panic!("Failed to read config file {}", e)
+    };
+
+    let config: Table = config_str.parse().unwrap();
+    // dbg!(&config);
+
+    let mut sensor = create_sensor(&config);
+
+    let (db_connection, db_table) = create_db_connection(&config);
+
+    let ticker = create_ticker(&config);
 
     let rt = Runtime::new().unwrap();
 
