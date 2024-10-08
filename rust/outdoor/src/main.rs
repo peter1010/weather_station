8d9ab289e335b1c47589939dfcc2316657377c48
Peter <peter@eowyn>
2024-10-08 20:56:00 +0100

    Code refactoring to reduce the size of the main function.x

--- rust/outdoor/src/main.rs
+++ rust/outdoor/src/main.rs
@@ -1,9 +1,10 @@
-//!
-//! Reading of Outdoor sensors
-//!
+///
+/// Reading of Outdoor sensors
+///
 
 use toml::Table;
 use tokio::time::sleep;
+use tokio::runtime::Runtime;
 use std::time::Duration;
 use std::sync::{Arc, Mutex};
 
@@ -15,13 +16,67 @@ use crate::wind::Wind;
 mod stats;
 mod wind;
 
+type Connection = Arc<Mutex<sqlite::Connection>>;
 
+//----------------------------------------------------------------------------------------------------------------------------------
+/// Aync wait for a tick event
 async fn wait_tick(ticker : &clock::Clock) -> Result<(), ()> {
-     let delay = ticker.secs_to_next_tick();
-     sleep(Duration::from_secs(delay.into())).await;
+     let delay_secs = ticker.secs_to_next_tick();
+     sleep(Duration::from_secs(delay_secs.into())).await;
      Ok(())
 }
 
+
+//----------------------------------------------------------------------------------------------------------------------------------
+/// Create a ticker
+fn create_ticker(config : &Table) -> clock::Clock {
+    let period_mins = config["common"]["sample_period_in_mins"].as_integer().unwrap() as i32;
+    clock::Clock::new(period_mins * 60)
+}
+
+
+//----------------------------------------------------------------------------------------------------------------------------------
+/// Create a database connection
+fn create_db_connection(config : &Table)-> (Connection, String) {
+
+    let db_file = config["outdoor"]["database"].as_str().unwrap();
+    println!("Opening database {}", db_file);
+    let db_connection = Arc::new(Mutex::new(sqlite::open(db_file).unwrap()));
+
+    let db_table = config["outdoor"]["db_table"].as_str().unwrap();
+    println!("Creating/using db table {}", db_table);
+
+    let query = format!("CREATE TABLE IF NOT EXISTS {} (unix_time INT NOT NULL, max REAL, ave REAL, min REAL,
+            PRIMARY KEY(unix_time));", db_table);
+    {
+        let conn = db_connection.lock().unwrap();
+        (*conn).execute(query).unwrap();
+    }
+    (db_connection, String::from(db_table))
+}
+
+
+//----------------------------------------------------------------------------------------------------------------------------------
+fn create_sensor(config : &Table) -> Arc<Wind> {
+
+    let dev_name = config["outdoor"]["wind_dev"].as_str().unwrap();
+    println!("Reading from {} for wind speeds", dev_name);
+
+    Arc::new(Wind::new(dev_name))
+}
+
+
+//----------------------------------------------------------------------------------------------------------------------------------
+fn launch_listener(config : &Table, rt : &Runtime, db_connection : Connection)
+{
+    let port = config["common"]["port"].as_integer().unwrap() as u16;
+    let mut listener = Listener::new(port, db_connection);
+
+    rt.spawn(async move { listener.task().await });
+}
+
+
+//----------------------------------------------------------------------------------------------------------------------------------
 /// Application entry point
 fn main() -> Result<(), ()> {
     let path = std::path::Path::new("weather.toml");
@@ -33,38 +88,19 @@ fn main() -> Result<(), ()> {
     let config: Table = config_str.parse().unwrap();
 //    dbg!(&config);
 
-    let db_file = config["outdoor"]["database"].as_str().unwrap();
-    println!("Opening database {}", db_file);
-    let db_connection = Arc::new(Mutex::new(sqlite::open(db_file).unwrap()));
+    let (db_connection, db_table) = create_db_connection(&config);
 
-    let db_table = config["outdoor"]["db_table"].as_str().unwrap();
-    println!("Creating/using db table {}", db_table);
+    let wind = create_sensor(&config);
 
-    let query = format!("CREATE TABLE IF NOT EXISTS {} (unix_time INT NOT NULL, max REAL, ave REAL, min REAL, PRIMARY KEY(unix_time));", db_table);
-    {
-        let conn = db_connection.lock().unwrap();
-        (*conn).execute(query).unwrap();
-    }
+    let ticker = create_ticker(&config);
 
-    let dev_name = config["outdoor"]["wind_dev"].as_str().unwrap();
-    println!("Reading from {} for wind speeds", dev_name);
-
-    let wind = Arc::new(Wind::new(dev_name));
-
-    let period = config["common"]["sample_period_in_mins"].as_integer().unwrap() as i32;
-    let ticker = clock::Clock::new(period * 60);
-
-    let rt = tokio::runtime::Runtime::new().unwrap();
+    let rt = Runtime::new().unwrap();
 
     let task_data = wind.clone();
 
     rt.spawn(async move { task_data.task().await });
 
-    let port = config["common"]["port"].as_integer().unwrap() as u16;
-    let mut listener = Listener::new(port, db_connection.clone());
-
-    rt.spawn(async move { listener.task().await });
-
+    launch_listener(&config, &rt, db_connection.clone());
 
     loop {
         rt.block_on(wait_tick(&ticker)).unwrap();
